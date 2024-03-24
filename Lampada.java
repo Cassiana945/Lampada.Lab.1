ba7d5e16cfb3a7c7877d08ebb9248a47cace3ac6
Cassiana <160614428+Cassiana945@users.noreply.github.com>
2024-03-24 12:37:57 -0300

    Create Lampada.java

---
+++ Lampada.java
@@ -0,0 +1,29 @@
+package br;
+
+public class Lampada {
+    private boolean status;
+    private String cor;
+    //construtor
+    public Lampada(boolean status) {
+        this.status = status;
+    }
+    // metodos get e set
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+}
+
+
